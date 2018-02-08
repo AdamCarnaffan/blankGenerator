337485b982cdc36425cab40ac9149ba45ee5b826
Adam Carnaffan <acarnaffan@hotmail.com>
2018-02-07 23:09:56 -0500

    Built User Class

--- class.php
+++ class.php
@@ -2,6 +2,23 @@
 
 class User {
   
+  public $id;
+  public $username;
+  public $teacher;
+  
+  public function __construct($data, $dbConn = null) {
+    if (is_numeric($data)) {
+      $getUser = "SELECT userID, username, isTeacher FROM users WHERE userID = '$data' LIMIT 1";
+      $userData = $dbConn->query($getUser)->fetch_array();
+    }
+    if (!is_array($data) && !is_array($userData)) {
+      throw new Exception("The User data stream could not be read");
+    }
+    $this->id = $userData['userID'];
+    $this->username = $userData['username'];
+    $this->teacher = ($userData['isTeacher'] == 1) ? true : false;
+  }
+  
 }
 
 class Quiz {
